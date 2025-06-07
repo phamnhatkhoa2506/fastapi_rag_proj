775b32c01243a98d03ad17767023b502edf80e57
Nhat Khoa <mizuneko2311@gmail.com>
2025-06-07 14:03:55 +0700

    hello

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 <h1>A simple project with FastAPI and RAG</h1>
+
+<h2>Steps to create a RAG project with LangChain</h2>
+
+<ol>
+    <li>
+        <h3>Indexing</h3>
+    </li>
+</ol>
+
--- src/app.py
+++ src/app.py
@@ -1,15 +1,36 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from langserve import add_routes
 
 from src.endpoints import test
 
 # Load environment variables
 load_dotenv()
 
+# Initial app
 app = FastAPI(
     title="A Simple Project With FastAPI and RAG",
     description="Here is the firt demo",
     version="1.0.0"
 )
 
+# Add middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
+
+# Include routes
 app.include_router(test.route)
+
+# langserve routes
+# add_routes(
+#     app,
+#     playground_type="default",
+#     path="/genai"
+# )
